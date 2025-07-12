2c37062f0d57376c0d052ba6c8663d573cc20dcb
Bablu C <bablu.c@hybb.in>
2025-07-12 11:21:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,17 @@ st.markdown("""
     .title {font-size: 32px; color: #006400; font-weight: bold; text-align: center; margin-top: 10px;}
     .company {font-size: 18px; text-align: center; color: white; margin-bottom: 20px; font-weight: bold;}
     .mismatch {background-color:#FFCDD2 !important;}
+    button[kind="primary"]:hover {
+        background-color: #228B22 !important;
+        transition: 0.3s ease;
+    }
 </style>
 """, unsafe_allow_html=True)
 
 st.markdown('<div class="title">HYBB Attendance System</div>', unsafe_allow_html=True)
-st.markdown('<div class="company">Hygiene Bigbite Pvt ltd</div>', unsafe_allow_html=True)
+st.markdown('<div class="company">Hygiene Bigbite Pvt Ltd</div>', unsafe_allow_html=True)
+st.markdown('<hr style="border:1px solid white; margin-top:0px">', unsafe_allow_html=True)
+st.markdown('<div style="text-align:center; color:white; font-size:14px; margin-top:40px;">© 2025 Hygiene Bigbite Pvt Ltd | All rights reserved</div>', unsafe_allow_html=True)
 
 # -------------------- GOOGLE AUTH --------------------
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -70,7 +76,7 @@ col1, col2 = st.columns([2, 1])
 with col1:
     st.subheader("Punch In / Punch Out")
     manager_list = ["", "Ayub Sait", "Rakesh Babu", "John Joseph", "Naveen Kumar M", "Sangeetha RM", "Joy Matabar", "Sonu Kumar", "Samsudeen", "Tauseef", "Bablu C", "Umesh M"]
-    kitchens = ["", "ANR01.BLR22", "BSK01.BLR19", "WFD01.BLR06", "MAR01.BLR05", "BTM01.BLR03", "IND01.BLR01", "HSR01.BLR02", "VDP01.CHN02", "MGP01.CHN01", "CMP01.CHN10", "KLN01.BLR09", "TKR01.BLR29", "CRN01.BLR17", "SKN01.BLR07", "HNR01.BLR16", "RTN01.BLR23", "YLK01.BLR15", "NBR01.BLR21", "PGD01.CHN06", "PRR01.CHN04", "FZT01.BLR20", "ECT01.BLR24", "SJP01.BLR08", "KPR01.BLR41", "BSN01.BLR40", "VNR01.BLR18", "SDP01.BLR34", "TCP01.BLR27", "BOM01.BLR04", "CK-Corp", "KOR01.BLR12"]
+    kitchens = ["", "ANR01.BLR22", "BSK01.BLR19", "WFD01.BLR06", "MAR01.BLR05", "BTM01.BLR03", "IND01.BLR01", "HSR01.BLR02", "VDP01.CHN02", "MGP01.CHN01", "CMP01.CHN10", "KLN01.BLR09", "TKR01.BLR29", "CRN01.BLR17", "SKN01.BLR07", "HNR01.BLR16", "RTN01.BLR23", "YLK01.BLR15", "NBR01.BLR21", "PGD01.CHN06", "PRR01.CHN04", "FZT01.BLR20", "ECT01.BLR24", "SJP01.BLR08", "KPR01.BLR41", "BSN01.BLR40", "VNR01.BLR18", "SDP01.BLR34", "TCP01.BLR27", "BOM01.BLR04", "CK-Corp"]
 
     with st.form("punch_form"):
         manager = st.selectbox("Select Manager", manager_list, key="manager")
